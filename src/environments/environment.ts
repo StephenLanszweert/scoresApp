1234f0578943e41f7cd09284111343529bf2c44e
StephenLanszweert <stephenlanszweert@hotmail.be>
2020-05-17 19:51:12 +0200

    Removed data in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,18 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-         production: false,
-         firebase: {
-           apiKey: "AIzaSyBYLXZCjuDEDFEYoxWOK1DB9OCy_kmOREs",
-           authDomain: "scoresapp-6fee2.firebaseapp.com",
-           databaseURL: "https://scoresapp-6fee2.firebaseio.com",
-           projectId: "scoresapp-6fee2",
-           storageBucket: "scoresapp-6fee2.appspot.com",
-           messagingSenderId: "635139457235",
-           appId: "1:635139457235:web:b6f26d9272ee4de2121426",
-           measurementId: "G-C5LP3XMM6N",
-         },
-       };
+  production: false,
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
