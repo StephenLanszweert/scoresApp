8f025c91c95930b58c5f4d20f5c0c31a7dec5f53
StephenLanszweert <stephenlanszweert@hotmail.be>
2020-05-18 23:09:41 +0200

    Added composers in firebase + added order by to compsers

--- src/app/components/composers/composers.component.ts
+++ src/app/components/composers/composers.component.ts
@@ -9,22 +9,16 @@ import { AngularFirestore } from "@angular/fire/firestore";
 })
 export class ComposersComponent implements OnInit {
   composers: any[];
-  items: Observable<any[]>;
   constructor(private dbService: AngularFirestore) {}
 
   ngOnInit() {
     this.dbService
-      .collection("/composers")
+      .collection("/composers", (ref) => ref.orderBy("name"))
       .valueChanges()
       .subscribe((data) => {
         console.log(data);
-        // this.items = data;
+        this.composers = data;
       });
-    console.log(this.items);
     // TODO: implement pagination for composers and parts
-    this.composers = [];
-    for (var i = 0; i < 50; i++) {
-      this.composers.push({ id: i, name: "Mozart" });
-    }
   }
 }
